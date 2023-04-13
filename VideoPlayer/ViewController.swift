13fb53c5e3eea5b8b1affd357befc895b0828e1f
Cristi Sandu <cristi.sandu@csx.ro>
2023-04-13 23:38:21 +0300

    Initial Commit

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Cristi Sandu on 13.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
